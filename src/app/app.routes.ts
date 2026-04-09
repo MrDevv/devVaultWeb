258253dca6040eab7f769b78fe3c975cc405c5c7
Miguel Angel Vega Perez <aries_242015@hotmail.com>
2026-04-09 18:03:25 -0500

    feat: agregar configuracion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginPage } from './auth/pages/login-page/login-page';
+import { HomePage } from './devVault-front/home/pages/home-page/home-page';
+import { RegisterPage } from './auth/pages/register/register-page';
+import { LandingPage } from './landing/pages/landing-page/landing-page';
+import { GuidePage } from './guide/pages/guide-page/guide-page';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPage
+    },
+    {
+        path: 'guide',
+        component: GuidePage
+    },
+    {
+        path: 'auth',
+        children: [
+            {
+                path: 'login',
+                component: LoginPage
+            },
+            {
+                path: 'register',
+                component: RegisterPage
+            }
+        ]
+    },
+    {
+        path: '',
+        children: [
+            {
+                path: 'home',
+                component: HomePage
+            },
+            {
+                path: '**',
+                redirectTo: 'home'
+            }
+        ]
+        
+    }
+
+];
---
+++ src/app/guide/pages/guide-page/guide-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guide-page',
+  imports: [],
+  templateUrl: './guide-page.html',
+  styleUrl: './guide-page.css',
+})
+export class GuidePage {}
