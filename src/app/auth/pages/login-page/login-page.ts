51aae458f49c1be23a2f87295dd3ec91d64b3e2b
Miguel Angel Vega Perez <aries_242015@hotmail.com>
2026-04-15 21:29:42 -0500

    feat: agregar página de login

--- src/app/auth/pages/login-page/login-page.ts
+++ src/app/auth/pages/login-page/login-page.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'login-page',
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
-export class LoginPage {}
+export class LoginPage {
+
+  router = inject(Router);
+  fb = inject(FormBuilder);
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  })
+
+  login(): void {
+    const { email, password } = this.loginForm.value
+    console.log( {email, password} );
+    
+    this.router.navigateByUrl("/home")
+  }
+
+}
