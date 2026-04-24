1092f7b6dd6f81286a051f434724f42436c242c0
Miguel Perez <94268929+MrDevv@users.noreply.github.com>
2026-04-24 16:09:40 -0500

    feat: agregar página professional data (#10)
    - se actualiza el componente sidebar con las opciones de administrador
    
    - se agrega la página de professional data consumiento el servicio

--- src/app/devVault-administrativa/devVault-administrativa.routes.ts
+++ src/app/devVault-administrativa/devVault-administrativa.routes.ts
@@ -6,6 +6,8 @@ import { NewExperience } from "./experience/pages/new-experience/new-experience"
 import { ListProjects } from "./projects/pages/list-projects/list-projects";
 import { NewProject } from "./projects/pages/new-project/new-project";
 import { DevVaultAdministrativaLayout } from "./shared/layouts/dev-vault-administrativa-layout/dev-vault-administrativ-layout";
+import { ProfessionalData } from "./professional-data/pages/professional-data/professional-data";
+import { EditProfessionalData } from "./professional-data/pages/edit-professional-data/edit-professional-data";
 
 export const routes: Routes = [
     {
@@ -15,6 +17,19 @@ export const routes: Routes = [
             {
                 path: 'home',
                 component: HomePage
+            },
+            {
+                path: 'professional-data',
+                children: [
+                    {
+                        path: '',
+                        component: ProfessionalData
+                    },
+                    {
+                        path: ':uuid',
+                        component: EditProfessionalData
+                    }
+                ]
             },
             {
                 path: 'experience',
