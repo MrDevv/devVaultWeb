0959bff410aea7d33bfcb14fda4ac0e01a5caac0
Miguel Angel Vega Perez <aries_242015@hotmail.com>
2026-04-24 16:07:21 -0500

    feat: agregar página professional data

--- src/app/devVault-administrativa/professional-data/pages/professional-data/professional-data.ts
+++ src/app/devVault-administrativa/professional-data/pages/professional-data/professional-data.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, ResourceRef } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { Developer } from '@devVault-administrativa/professional-data/interfaces/Developer';
+import { ProfessionalDataService } from '@devVault-administrativa/professional-data/services/professional-data-service';
+import { NavbarAdministrativo } from "@devVault-administrativa/shared/components/navbar-administrativo/navbar-administrativo";
+import { APIResponse } from '@shared/interfaces/APIResponse';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'professional-data',
-  imports: [],
+  imports: [NavbarAdministrativo, RouterLink],
   templateUrl: './professional-data.html'  
 })
-export class ProfessionalData {}
+export class ProfessionalData {
+  private professionalService = inject(ProfessionalDataService);
+
+  get professionalData(): Developer | undefined {
+    return this.professionalDataResource.value()?.data?.[0];
+  }
+
+  professionalDataResource: ResourceRef<APIResponse<Developer[]> | undefined> = rxResource({
+    stream: () =>{
+      return this.professionalService.obtenerDatosProfesionales();
+    }
+  })
+}
