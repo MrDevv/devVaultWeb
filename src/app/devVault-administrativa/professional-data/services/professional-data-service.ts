451cb5562c7f2d2753b12aca88703b6df0215b2b
Miguel Angel Vega Perez <aries_242015@hotmail.com>
2026-04-24 19:52:08 -0500

    feat: agregar data caché en el servicio professional data

--- src/app/devVault-administrativa/professional-data/services/professional-data-service.ts
+++ src/app/devVault-administrativa/professional-data/services/professional-data-service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { environment } from '@environments/environment';
+import { inject, Injectable, signal } from '@angular/core';
+import { catchError, Observable, of, tap, throwError } from 'rxjs';
+
 import { APIResponse } from '@shared/interfaces/APIResponse';
 import { Developer } from '@professional-data/interfaces/Developer'
-import { catchError, throwError } from 'rxjs';
+import { environment } from '@environments/environment';
 
 const BASEURL = environment.API_URL;
 
@@ -13,8 +14,16 @@ const BASEURL = environment.API_URL;
 export class ProfessionalDataService {
   private _http = inject(HttpClient)
 
-  public obtenerDatosProfesionales() {
+  private professionalDataCache = signal<APIResponse<Developer[]> | null>(null);
+
+  public obtenerDatosProfesionales(): Observable<APIResponse<Developer[]>> {
+
+    if (this.professionalDataCache()!= null) {
+      return of(this.professionalDataCache()!);
+    }
+
     return this._http.get<APIResponse<Developer[]>>(`${BASEURL}/desarrolladores`).pipe(
+      tap(resp => this.professionalDataCache.set(resp)),
       catchError((error) => throwError(() => error.error))
     )
   }
