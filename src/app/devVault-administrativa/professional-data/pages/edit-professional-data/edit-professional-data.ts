0fbdab9ed1c5027a269c550e273d10a924ab7529
Miguel Perez <94268929+MrDevv@users.noreply.github.com>
2026-04-28 16:09:12 -0500

    feat(professional-data): agregar funcionalidad actualizar datos profesionales (#13)

--- src/app/devVault-administrativa/professional-data/pages/edit-professional-data/edit-professional-data.ts
+++ src/app/devVault-administrativa/professional-data/pages/edit-professional-data/edit-professional-data.ts
@@ -1,19 +1,23 @@
-import { Component, effect, inject, OnInit, ResourceRef } from '@angular/core';
+import { Component, effect, inject, ResourceRef, signal } from '@angular/core';
 import { Developer } from '@devVault-administrativa/professional-data/interfaces/Developer';
 import { APIResponse } from '@shared/interfaces/APIResponse';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ProfessionalDataService } from '@devVault-administrativa/professional-data/services/professional-data-service';
-import { FormBuilder, ReactiveFormsModule, Validators, ɵInternalFormsSharedModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
+import { firstValueFrom } from 'rxjs';
+import { Loader } from "@shared/components/loader/loader";
 
 @Component({
   selector: 'edit-professional-data',
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, Loader],
   templateUrl: './edit-professional-data.html'  
 })
 export class EditProfessionalData {
   public professionalDataService = inject(ProfessionalDataService)
   private _fb = inject(FormBuilder);
+  public isLoading = signal(false);
 
   formProfessionalData = this._fb.group({
     nombres: ['', Validators.required],
@@ -25,8 +29,8 @@ export class EditProfessionalData {
     linkedin_url: [''],
     cv_url: [''],
     prefijo_telefono: ['', Validators.pattern('^[0-9]+$')],
-    telefono: ['', Validators.pattern('^[0-9]+$')]
-
+    telefono: ['', Validators.pattern('^[0-9]+$')],
+    correo_contacto: ['', Validators.email]
   })
 
   constructor(){
@@ -47,9 +51,27 @@ export class EditProfessionalData {
     }
   })
 
-  editarDatos() {
-    console.log(this.formProfessionalData.value);
-    
+  async editarDatos() {
+    this.isLoading.set(true);
+    const developerUpdate:Developer = {
+      ...(this.formProfessionalData.value as any)
+    };
+
+    try {
+      await firstValueFrom(this.professionalDataService.actualizarDatosProfesionales(developerUpdate));
+      Swal.fire({
+        icon: "success",
+        title: "Éxito",
+        text: "Se actualizarón tus datos correctamente."
+      });
+    } catch (error: any) {
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: error.message
+      });
+    }
+    this.isLoading.set(false);
   }
 
 }
